26aa814162cf5c85d08be538b41dafbf5753a90f
Joakim Uddholm <tethik@blacknode.se>
2020-11-20 22:25:11 +0100

    fix broken dependency -.-

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ install_requires     = ['paramiko>=1.15.2',
                         'psutil>=3.3.0',
                         'intervaltree>=3.0',
                         'sortedcontainers',
-                        'unicorn>=1.0.2rc1,<1.0.2rc4', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
+                        'unicorn>=1.0.2', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
                         'six>=1.12.0',
                         'rpyc',
 ]
